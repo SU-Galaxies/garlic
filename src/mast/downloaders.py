c214c62668b72f98e82499268e833efd78726ecf
Axel Runnholm <axel.runnholm@astro.su.se>
2022-08-26 13:34:28 +0200

    Add mast downloader script

---
+++ src/mast/downloaders.py
@@ -0,0 +1,38 @@
+"""Python script to download data from MAST staging areas"""
+import argparse
+from ftplib import FTP_TLS
+import getpass
+
+
+def mastftp(user, stage, suffix):
+    print("Connecting to MAST as user: " + user)
+    passw = getpass.getpass()
+    ftps = FTP_TLS("archive.stsci.edu")
+    ftps.login(user=user, passwd=passw)
+    ftps.prot_p()
+    ftps.cwd("stage/" + stage)
+
+    filenames = ftps.nlst()
+    if suffix is None:
+        suffix = ""
+    for fn in filenames:
+        if fn.endswith(suffix):
+            print("getting " + fn)
+            with open(fn, "wb") as fp:
+                ftps.retrbinary("RETR {}".format(fn), fp.write)
+
+
+# -----------------------------------------------------------------------------
+# Script I/O
+# -----------------------------------------------------------------------------
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawTextHelpFormatter,
+        fromfile_prefix_chars="@",
+    )
+    parser.add_argument("user")
+    parser.add_argument("stage")
+    parser.add_argument("--suffix", default=None)
+    args = parser.parse_args()
+    maastftp(args.user, args.stage, args.suffix)
